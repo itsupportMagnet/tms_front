e5a0c20557e7efa507eaa327f949d0de63e57d73
JosiasSalgadoBetterCommerce <josias.salgado@bettercommerce.cl>
2023-12-11 13:50:26 -0700

    change

---
+++ src/views/AllOperations.vue
@@ -0,0 +1,8 @@
+<template>
+  <AllOperationTable :operations="operations"/>
+</template>
+
+<script setup>
+import AllOperationTable from '../components/Tables/AllOperationTable/AllOperationTable.vue';
+
+</script>
